f31bb1953d21cbbf45960682310563986a9a5cd2
Emilia Kond <emilia@rymiel.space>
2024-09-16 19:28:54 +0300

    another kind of rich text element

--- src/components/richText.tsx
+++ src/components/richText.tsx
@@ -7,20 +7,25 @@ import { uri } from "..";
 const LINK_WRAPPER = /(\[[^\]]+\])/g;
 const SIMPLE_LINK = /\[([A-Za-z0-9_-]+)\]/;
 const COMPLEX_LINK = /\[([A-Za-z0-9_-]+)\(([^)]+)\)\]/;
+const NON_LINK = /\[\(([^)]+)\)\]/;
 
 export function RichText({ text }: { text: string }) {
   const { entries } = useContext(Dictionary);
   const highlighted = reactStringReplace(text, LINK_WRAPPER, (m) => {
     let id;
     let label = null;
-    let c = COMPLEX_LINK.exec(m);
-    let s = SIMPLE_LINK.exec(m);
+    const c = COMPLEX_LINK.exec(m);
+    const s = SIMPLE_LINK.exec(m);
     if (c !== null) {
       id = c[1];
       label = c[2];
     } else if (s != null) {
       id = s[1];
     } else {
+      const n = NON_LINK.exec(m);
+      if (n != null) {
+        return <i>{n[1]}</i>;
+      }
       return m;
     }
 
