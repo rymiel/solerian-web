fecc0d5a3019c0e17b4a1c920d970d8e93e5e4ee
Emilia Kond <emilia@rymiel.space>
2024-09-17 20:46:39 +0300

    Improve reverse lookup

--- src/page/ReversePage.tsx
+++ src/page/ReversePage.tsx
@@ -1,10 +1,10 @@
 import { Button, ControlGroup, InputGroup, NonIdealState, Spinner, SpinnerSize } from "@blueprintjs/core";
-import { useContext, useState } from "react";
+import { memo, useContext, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { App } from "../App";
 import { Dictionary, FullEntry } from "../dictionary";
 import { Part } from "../lang/extra";
-import { FORM_NAMES } from "../lang/inflection";
+import { applyNormalize, FORM_NAMES } from "../lang/inflection";
 import { uri } from "..";
 import { InflEntry, useInflEntries } from "../lang/inflEntries";
 
@@ -39,6 +39,119 @@ function inflNode(entry: InflEntry) {
   );
 }
 
+function echo(original: string, cut: string, children: React.ReactNode[]) {
+  return (
+    <>
+      <i>{original}</i>: echo prefixed form of <i>{cut}</i>
+      <ul>
+        {children.map((i, j) => (
+          <li key={j}>{i}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function poss(original: string, cut: string, form: string, children: React.ReactNode[]) {
+  return (
+    <>
+      <i>{original}</i>: {form} possessive of <i>{cut}</i>
+      <ul>
+        {children.map((i, j) => (
+          <li key={j}>{i}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+const POSS_SUFFIXES = {
+  cur: ["àl", "it", "ys", "erd", "itar", "usd"],
+  old: ["elm", "etr", "usd", "usan", "ys", "elmes", "etres", "usdes"],
+} as const;
+const POSS_FORMS = {
+  cur: ["1sg", "2sg", "3sg", "1pl", "2pl", "3pl"],
+  old: ["1sg", "2sg", "3sg masculine", "3sg feminine", "3sg neuter", "1pl", "2pl", "3pl"].map((i) => `old ${i}`),
+} as const;
+
+const ReverseContent = memo(function ReverseContent({
+  infl,
+  raw,
+  query,
+  includeOld,
+}: {
+  infl: InflEntry[];
+  raw: FullEntry[];
+  query: string;
+  includeOld: boolean;
+}) {
+  const lookup = (q: string, { verbOnly = false, oldEquals }: { verbOnly?: boolean; oldEquals?: boolean } = {}) => {
+    let inflMatches = infl.filter(
+      (i) =>
+        i.sol === q &&
+        (!i.old || includeOld) &&
+        i.form !== 0 &&
+        (!verbOnly || i.original.part === Part.Verb) &&
+        (oldEquals === undefined || oldEquals === i.old)
+    );
+    let rawMatches = raw.filter((i) => i.sol === q && (!verbOnly || i.part === Part.Verb));
+
+    return rawMatches.map(terminal).concat(inflMatches.map(inflNode));
+  };
+
+  const r = lookup(query);
+
+  if (applyNormalize(query) === query) {
+    if (query.startsWith("fy") && query.length > 2 && !"aeiouyàáéíóúý".includes(query[2])) {
+      const cut = applyNormalize(query.slice(2));
+      const rc = lookup(cut, { verbOnly: true });
+      if (rc.length > 0) {
+        r.push(echo(query, cut, rc));
+      }
+    }
+
+    if (query.startsWith("fyn") && query.length > 3 && "aeiouyàáéíóúý".includes(query[3])) {
+      const cut = applyNormalize(query.slice(3));
+      const rc = lookup(cut, { verbOnly: true });
+      if (rc.length > 0) {
+        r.push(echo(query, cut, rc));
+      }
+    }
+
+    POSS_SUFFIXES.cur.forEach((suffix, i) => {
+      if (query.endsWith(suffix)) {
+        const cut = applyNormalize(query.slice(0, -suffix.length));
+        const rc = lookup(cut, { oldEquals: false });
+        if (rc.length > 0) {
+          r.push(poss(query, cut, POSS_FORMS.cur[i], rc));
+        }
+      }
+    });
+
+    POSS_SUFFIXES.old.forEach((suffix, i) => {
+      if (query.endsWith(suffix)) {
+        const cut = applyNormalize(query.slice(0, -suffix.length));
+        const rc = lookup(cut, { oldEquals: true });
+        if (rc.length > 0) {
+          r.push(poss(query, cut, POSS_FORMS.old[i], rc));
+        }
+      }
+    });
+  }
+
+  if (r.length === 0) {
+    return <NonIdealState icon="cross-circle" title="No results" />;
+  } else {
+    return (
+      <ul>
+        {r.map((i, j) => (
+          <li key={j}>{i}</li>
+        ))}
+      </ul>
+    );
+  }
+});
+
 export default function ReversePage() {
   const { entries } = useContext(Dictionary);
   const { query } = useParams();
@@ -52,28 +165,8 @@ export default function ReversePage() {
   if (query === undefined) {
     content = <NonIdealState icon="search" />;
   } else if (entries && infl) {
-    // TODO: posessive forms
-    const lookup = (q: string) => {
-      const inflMatches = infl.filter((i) => i.sol === q && (!i.old || includeOld) && i.form !== 0);
-      const rawMatches = entries.filter((i) => i.sol === q);
-
-      return rawMatches.map(terminal).concat(inflMatches.map(inflNode));
-    };
-
     // TODO: multiple words
-    const r = lookup(query);
-
-    if (r.length === 0) {
-      content = <NonIdealState icon="cross-circle" title="No results" />;
-    } else {
-      content = (
-        <ul>
-          {r.map((i, j) => (
-            <li key={j}>{i}</li>
-          ))}
-        </ul>
-      );
-    }
+    content = <ReverseContent key="content" infl={infl} raw={entries} query={query} includeOld={includeOld} />;
   }
 
   return App(
