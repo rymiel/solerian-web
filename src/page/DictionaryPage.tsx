251e9888417696879810f0c62ced85111174433b
Emilia Kond <emilia@rymiel.space>
2024-09-20 03:36:50 +0300

    Make dictionary rows middle clickable

--- src/page/DictionaryPage.tsx
+++ src/page/DictionaryPage.tsx
@@ -29,33 +29,51 @@ export default function DictionaryPage() {
           </thead>
           <tbody>
             {entries.map((e, i) => (
-              <tr key={e.hash} onClick={() => navigate(uri`/w/${e.sol}`)}>
-                <td>{i + 1}</td>
+              <tr key={e.hash}>
                 <td>
-                  {e.tag && <Tag intent="danger">{e.tag}</Tag>}{" "}
-                  {e.meanings.map((m, mi) => (mi === 0 ? "" : "; ") + m.eng)}
-                  {e.meanings.some((m) => m.sections.some((s) => s.title === SectionTitle.TRANSLATION)) && (
-                    <Icon icon="label" title="has a translation" />
-                  )}
-                  {e.sections.some((s) => s.title === SectionTitle.USAGE) && (
-                    <Icon icon="info-sign" title="has usage notes" />
-                  )}
-                  {e.sections.some((s) => s.title === SectionTitle.ETYMOLOGY) && (
-                    <Icon icon="book" title="has etymology" />
-                  )}
-                  {e.sections.some((s) => s.title === SectionTitle.INSTEAD) && (
-                    <Icon icon="flow-end" title="has another word that should be used instead" intent="danger" />
-                  )}
-                </td>
-                <td className="dual">
-                  <i>{e.sol}</i>
-                  <span className="sol">{e.script}</span>
+                  <a href={uri`#/w/${e.sol}`} className="link-fill">
+                    <span>{i + 1}</span>
+                  </a>
                 </td>
                 <td>
-                  {e.extra}
-                  {e.class && `-${e.class}`}
+                  <a href={uri`#/w/${e.sol}`} className="link-fill">
+                    <span>
+                      {e.tag && <Tag intent="danger">{e.tag}</Tag>}{" "}
+                      {e.meanings.map((m, mi) => (mi === 0 ? "" : "; ") + m.eng)}
+                      {e.meanings.some((m) => m.sections.some((s) => s.title === SectionTitle.TRANSLATION)) && (
+                        <Icon icon="label" title="has a translation" />
+                      )}
+                      {e.sections.some((s) => s.title === SectionTitle.USAGE) && (
+                        <Icon icon="info-sign" title="has usage notes" />
+                      )}
+                      {e.sections.some((s) => s.title === SectionTitle.ETYMOLOGY) && (
+                        <Icon icon="book" title="has etymology" />
+                      )}
+                      {e.sections.some((s) => s.title === SectionTitle.INSTEAD) && (
+                        <Icon icon="flow-end" title="has another word that should be used instead" intent="danger" />
+                      )}
+                    </span>
+                  </a>
                 </td>
-                <td>{e.ipa}</td>
+                <td>
+                  <a href={uri`#/w/${e.sol}`} className="link-fill dual">
+                    <i>{e.sol}</i>
+                    <span className="sol">{e.script}</span>
+                  </a>
+                </td>
+                <td>
+                  <a href={uri`#/w/${e.sol}`} className="link-fill">
+                    <span>
+                      {e.extra}
+                      {e.class && `-${e.class}`}
+                    </span>
+                  </a>
+                </td>
+                <td>
+                  <a href={uri`#/w/${e.sol}`} className="link-fill">
+                    <span>{e.ipa}</span>
+                  </a>
+                </td>
               </tr>
             ))}
           </tbody>
