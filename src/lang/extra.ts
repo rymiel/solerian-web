e65e60e1e33a00be0252526e5dae75e1712f30c9
Emilia Kond <emilia@rymiel.space>
2024-10-18 12:47:24 +0300

    Proper names for patterns

--- src/lang/extra.ts
+++ src/lang/extra.ts
@@ -31,7 +31,64 @@ const SUFFIXES = {
   ],
   [Part.Pronoun]: [],
 } as const;
-export type Types = { [P in Part]: (typeof SUFFIXES)[P][number][1] };
+export type Types = { readonly [P in Part]: (typeof SUFFIXES)[P][number][1] };
+
+type PatternNames = readonly [cur: string, old: string, quant: string];
+type PatternNameTable = {
+  readonly [P in Part]: { readonly [T in Types[P]]: PatternNames };
+};
+const PATTERN_NAME_TABLE: PatternNameTable = {
+  [Part.Noun]: {
+    "1": ["1", "F1t", "Formerly feminine"],
+    "2": ["2", "F1d", "Formerly feminine"],
+    "3": ["3", "F2i", "Formerly feminine"],
+    "4": ["4", "F2x", "Formerly feminine"],
+    "5": ["5", "F2", "Formerly feminine"],
+    "6": ["6", "M1", "Formerly masculine"],
+    "7": ["7", "M2", "Formerly masculine"],
+    "8": ["8", "N1", "Formerly neuter"],
+    "9": ["9", "N2", "Formerly neuter"],
+  },
+  [Part.Verb]: {
+    "1": ["1", "I", "Intransitive continuous"],
+    "2": ["2", "II", "Transformation"],
+    "2n": ["2", "II", "Transformation"],
+    "3": ["3", "III", "One-time action"],
+    "3n": ["3n", "III", "One-time action"],
+    "3r": ["3r", "III", "One-time action"],
+    "4": ["4", "IV", "Adjective"],
+    "4s": ["4s", "IV", "Adjective"],
+    "5": ["5", "0", "Transitive continuous"],
+    "5r": ["5r", "0", "Transitive continuous"],
+    "5t": ["5t", "0", "Transitive continuous"],
+  },
+  [Part.Pronoun]: {},
+};
+
+export function patternNames(word: string, part: Part): PatternNames | null {
+  const f = <P extends Part>(p: P) => {
+    const t = determineType(word, p);
+    return t === null ? null : PATTERN_NAME_TABLE[p][t];
+  };
+
+  // prettier-ignore
+  switch (part) {
+    case Part.Noun: return f(part);
+    case Part.Verb: return f(part);
+    case Part.Pronoun: return f(part);
+  }
+}
+
+export function patternDescriptors(word: string, part: Part): PatternNames | null {
+  const names = patternNames(word, part);
+  if (names === null) return null;
+
+  return [
+    `Pattern ${names[0]} ${Part[part].toLowerCase()}`,
+    `Old class ${names[1]} ${Part[part].toLowerCase()}`,
+    `${names[2]}`,
+  ];
+}
 
 export function partOfExtra(extra: string): Part | null {
   if (extra.startsWith("N")) {
@@ -45,6 +102,7 @@ export function partOfExtra(extra: string): Part | null {
   }
 }
 
+export function determineType<P extends Part>(word: string, part: P): Types[P] | null;
 export function determineType(word: string, part: Part): Types[Part] | null {
   const types = SUFFIXES[part];
   for (const [suffix, t] of types) {
